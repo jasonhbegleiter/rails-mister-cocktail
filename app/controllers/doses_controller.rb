ac70da5a7fa1dfcebc702337ccae2369629e092b
Jason Begleiter <jhbegs204@gmail.com>
2018-05-30 14:23:26 +0300

    Working end to end

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+  def new
+    @ingredients = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new({cocktail: @cocktail})
+  end
+
+  def create
+    # raise
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+    redirect_to cocktail_path(params[:cocktail_id])
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
